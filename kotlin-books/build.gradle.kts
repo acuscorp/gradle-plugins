056aa303b0a3467294850a490391478672cf6879
acuscorp <acuscorp@gmail.com>
2022-03-22 08:59:26 -0600

    remove buildSrc and fix build logic

--- kotlin-books/build.gradle.kts
+++ kotlin-books/build.gradle.kts
@@ -1,8 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-  kotlin("jvm")
-}
 subprojects{
     
   repositories {
@@ -12,18 +8,4 @@ subprojects{
 
   apply(from = rootProject.file("gradle/licensing.gradle"))
   apply(plugin = "kotlin-conventions")
-}
-dependencies {
-  implementation(kotlin("stdlib-jdk8"))
-}
-repositories {
-  mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-  jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-  jvmTarget = "1.8"
 }
